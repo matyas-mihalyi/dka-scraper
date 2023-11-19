53984014559fb60343e1c99556cf4f79cf32ff4c
Matyas Mihalyi <mihalyi.istvan.m@outlook.hu>
2023-11-19 21:03:18 +0100

    update env variable names

--- src/db.js
+++ src/db.js
@@ -3,10 +3,10 @@ import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize({
   dialect: 'postgres',
-  host: process.env.PGHOST,
-  port: +process.env.PGPORT,
-  username: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  host: process.env.SCRAPER_DB_HOST,
+  port: +process.env.SCRAPER_DB_PORT,
+  username: process.env.SCRAPER_DB_USERNAME,
+  password: process.env.SCRAPER_DB_PASSWORD,
+  database: process.env.SCRAPER_DB_NAME,
   logging: false
 });
