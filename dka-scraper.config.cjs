f20e1a1fa2ae668c5bdbcca9f96d01a7b9dd78aa
Matyas Mihalyi <mihalyi.istvan.m@outlook.hu>
2024-12-15 22:06:10 +0100

    remove watch flag
    Watch flag was making pm2 restart the app because error logs were written to files.

--- dka-scraper.config.cjs
+++ dka-scraper.config.cjs
@@ -1,10 +1,11 @@
 module.exports = {
-  apps : [{
-    name   : "DKA_SCRAPER",
-    script : "npm",
-    args : "run start --prefix /home/dka/scraper",
-    watch : true,
-    cron: "0 3 * * *",
-    autorestart: false
-  }]
-}
+  apps: [
+    {
+      name: "DKA_SCRAPER",
+      script: "npm",
+      args: "run start --prefix /home/dka/scraper",
+      cron: "0 3 * * *",
+      autorestart: false,
+    },
+  ],
+};
